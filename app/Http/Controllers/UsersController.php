3d611348a141bb879dca9e65e06c701974ee5788
TecJP <tec.joao.freitas@gmail.com>
2019-07-26 21:52:28 -0300

    Adição das funções de edição, cadastro e exclusão

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -13,6 +13,11 @@ class UsersController extends Controller
         return view('aplication.index', compact('usuarios'));
     }
 
+    public function formCadastro()
+    {
+        return view('aplication.new_user');
+    }
+
     public function cadastro(Request $request)
     {
         $usuario = new Usuario();
@@ -21,6 +26,40 @@ class UsersController extends Controller
         $usuario->wins = $request->wins;
         $usuario->loses = $request->loses;
         $usuario->save();
-        return redirect()->to('aplication/index/');
+        return redirect()->to('aplication');
     }
+
+    public function formEditar($id)
+    {
+        $usuario = Usuario::find($id);
+        if (!$usuario) {
+            abort(404);
+        }
+        return view ('aplication.edit', compact('usuario'));
+    }
+
+    public function editar(Request $request, $id)
+    {
+        $usuario = Usuario::find($id);
+        if (!$usuario) {
+            abort(404);
+        }
+        $usuario->name = $request->name;
+        $usuario->deck = $request->deck;
+        $usuario->wins = $request->wins;
+        $usuario->loses = $request->loses;
+        $usuario->save();
+        return redirect()->to('aplication');
+    }
+
+    public function excluir(Request $request, $id)
+    {
+        $usuario = Usuario::find($id);
+        if (!$usuario){
+            abort(404);
+        }
+        $usuario->delete();
+        return redirect()->to('aplication');
+    }
+
 }
